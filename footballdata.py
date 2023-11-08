3355c96ebaf746b4443ce693027167ac982958a8
Keebinlam <140448996+Keebinlam@users.noreply.github.com>
2023-11-08 13:32:18 -0700

    Finished code

--- footballdata.py
+++ footballdata.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
+#Extracting a CVS file from a website.
 url = 'https://www.football-data.co.uk/mmz4281/2324/E0.csv'
 df = pd.read_csv(url)
+
+
+# renaming 2 columns 
+df.rename(columns={'FTHG': 'Final Time Home Goals',
+          'FTAG': 'Final Time Away Goals'}, inplace=True)
+#showing table with new columns 
 print(df)
